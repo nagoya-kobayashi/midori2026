2cc1d4392677616fcc7e50bfc477bf27c9d645b3
nagoya-kobayashi <kobayashi.yu@nagoya-c.ed.jp>
2026-04-28 09:46:13 +0900

    Add files via upload

---
+++ crossword/config.js
@@ -0,0 +1,5 @@
+window.CROSSWORD_CONFIG = {
+  gasUrl: "https://script.google.com/macros/s/AKfycbyGIpXP5aAPy0tnoLwCl7c8arJSNIP17Q45SaXbIfaKEjQN74YD3A4jOijbtnkak_g/exec",
+  pollMinMs: 20000,
+  pollMaxMs: 30000
+};
